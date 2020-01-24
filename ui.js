b3a1aa1cca396660982a54bf50354280baa2cae3
x <x@X-PC>
2020-01-24 23:00:55 +0200

    Update

--- ui.js
+++ ui.js
@@ -1,28 +1,36 @@
+function check(isNull) {
+  if (isNull != null) {
+    return isNull;
+  } else {
+    return "unknown";
+  }
+}
+
 class UI {
   constructor() {
     this.profile = document.getElementById("profile");
   }
   showProfile(user) {
-    //console.log(user);
+    //console.log(user.name);
     this.profile.innerHTML = `
     <div class="card card-body mb-3">
-      <h1 class="text-center bg-light mb-3 p-4" style="font-weight:bold;text-transform:uppercase;">${user.name}</h1>
+      <h1 class="text-center bg-light mb-3 p-4" style="font-weight:bold;text-transform:uppercase;">${check(user.name)}</h1>
       <div class="row">
          <div class="col-md-3">
-            <img class="img-fluid mb-2" src="${user.avatar_url}"/>
-            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+            <img class="img-fluid mb-2" src="${check(user.avatar_url)}"/>
+            <a href="${check(user.html_url)}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
          </div>
          <div class="col-md-9">
-            <span class="badge badge-primary">Public Repos ${user.public_repos}</span>
-            <span class="badge badge-secondary">Public Gists ${user.public_gists}</span>
-            <span class="badge badge-success">Followers: ${user.followers}</span>
-            <span class="badge badge-info">Following ${user.following}</span>
+            <span class="badge badge-primary">Public Repos ${check(user.public_repos)}</span>
+            <span class="badge badge-secondary">Public Gists ${check(user.public_gists)}</span>
+            <span class="badge badge-success">Followers: ${check(user.followers)}</span>
+            <span class="badge badge-info">Following ${check(user.following)}</span>
             <br><br>
             <ul class="list-group">
-               <li class="list-group-item">Company: ${user.company}</li>
-               <li class="list-group-item">Website/Blog: ${user.blog}</li>
-               <li class="list-group-item">Location: ${user.location}</li>
-               <li class="list-group-item">Member since: ${user.created_at}</li>
+               <li class="list-group-item">Company: ${check(user.company)}</li>
+               <li class="list-group-item">Website/Blog: ${check(user.blog)}</li>
+               <li class="list-group-item">Location: ${check(user.location)}</li>
+               <li class="list-group-item">Member since: ${check(user.created_at)}</li>
             </ul>
          </div>
       </div>
@@ -38,12 +46,12 @@ class UI {
            <div class="card card-body mb-2">
                <div class="row">
                   <div class="col-md-6">
-                     <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                     <a href="${check(repo.html_url)}" target="_blank">${check(repo.name)}</a>
                   </div>
                   <div class="col-md-6">
-                     <span class="badge badge-secondary">Stars: ${repo.stargazers_count}</span>
-                     <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
-                     <span class="badge badge-secondary">Forks ${repo.forks}</span>
+                     <span class="badge badge-secondary">Stars: ${check(repo.stargazers_count)}</span>
+                     <span class="badge badge-secondary">Watchers: ${check(repo.watchers_count)}</span>
+                     <span class="badge badge-secondary">Forks ${check(repo.forks)}</span>
                   </div>
                </div>
            </div> 
